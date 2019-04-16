7cba13cd667d794dfe35770603d22af516660045
Rafael Carvalho <contato.rdev@gmail.com>
2019-04-16 09:33:52 -0300

    fixed path

--- main.sh
+++ main.sh
@@ -12,7 +12,7 @@ function success() {
   printf "\n${GREEN}%s${END}\n" "$*"
 }
 
-path_to_script=${PWD}
+path_to_script="${PWD}/${current_dir}"
 current_dir=$(dirname $0)
 
 if [[ "$parameters" = *"--path"* ]]; then
@@ -24,5 +24,5 @@ if [[ "$parameters" = *"--port"* ]]; then
 fi
 
 info "Opening python webserver..."
-python "${current_dir}/src/main.py" ${port}
+python "${path_to_script}/src/main.py" ${port}
 success "Done!"
